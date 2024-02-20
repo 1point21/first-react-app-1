9adeb83ccf6ee337a603af5fa2b2bccb70512afc
1point21 <j.drinkall@mailfence.com>
2024-02-20 17:36:29 +0000

    feat: add listners testing

--- src/Content.js
+++ src/Content.js
@@ -8,11 +8,21 @@ const Content = () => {
         const int = Math.floor(Math.random() * 3)
         return names[int]
       }
+
+      const handleClick = () => {
+        console.log("You clicked it")
+      }
+      const handleClickTwo = (name) => {
+        console.log(`${name} was clicked!`)
+      }
     return (
     <main>
       <p>
         Hello {handleNameChange()}
       </p>
+      <button onClick={handleClick}>Click it</button>
+      {/* this needs to be inside an anonymous function else it will be called immediately */}
+      <button onClick={() => {handleClickTwo('James')}}>Click it here too!</button>
     </main>
   )
 }
