f54b216325a20143b25c0f8a12b05afb8a5ae0fe
1point21 <j.drinkall@mailfence.com>
2024-02-20 17:36:12 +0000

    feat: add inline css for demonstration

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 const Header = () => {
+  // this is a way to define inline styles
+  // then call under "<header style={headerStyle}>"
+  const headerStyle = {
+    backgroundColor: "mediumblue",
+    color: "#fff",
+  };
+
   return (
     <header>
       <h1>Groceries List</h1>
