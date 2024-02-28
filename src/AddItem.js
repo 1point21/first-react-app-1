b5991478d67fe492ed8cdb87d25dcf50816f2f4c
1point21 <j.drinkall@mailfence.com>
2024-02-28 11:57:11 +0000

    feat: add functionality for additem form

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,20 +1,31 @@
 import React from "react";
 import { FaPlus } from 'react-icons/fa'
+import { useRef } from 'react'
 
 const AddItem = ({ newItem, setNewItem, handleSubmit}) => {
-  return (
+  const inputRef = useRef()
+  
+    return (
+    // call handleSubmit function when form submitted
     <form className="addForm" onSubmit={handleSubmit}>
       <label htmlFor="addItem">Add Item</label>
       <input
         autoFocus
+        ref={inputRef}
         id="addItem"
         type="text"
         placeholder="Add Item"
         required
+        // controlled component:
+        // value is initially newItem, which is initially empty string
+        // (see app.js file)
         value={newItem}
+        // event handler updates the state of newItem using the setter
+        // as the user types in the box
+        // as typed in the box, set the state as the typed value
         onChange={(e) => setNewItem(e.target.value)}
       />
-      <button type="submit" aria-label="Add Item">
+      <button type="submit" aria-label="Add Item" onClick={() => inputRef.current.focus()}>
         <FaPlus/>
       </button>
     </form>
--- src/App.js
+++ src/App.js
@@ -3,61 +3,63 @@ import Content from "./Content";
 import Footer from "./Footer";
 import { useState } from "react";
 import AddItem from "./AddItem";
+import SearchItem from "./SearchItem";
 
 // modern react uses functional components
 // this is the parent component
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "One half pound bag of nuts",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3",
-    },
-  ]);
+  // SET STATES
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("shoppinglist"))
+  );
 
   const [newItem, setNewItem] = useState("");
 
-  const addItem = (item) => {
-    const id = items.length ? items[items.length -1].id + 1 : 1
-    const myNewItem = {
-      id, checked: false, item
-    }
-    const listItems = [...items, myNewItem]
-    setNewItem(listItems)
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems))
-  }
+  const [search, setSearch] = useState("");
 
+  // helper function for setting new items/state AND store in local storage
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
+  };
+
+  // create new item for list
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = {
+      id,
+      checked: false,
+      item,
+    };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  };
+
+  // remove/add check to checkbox
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   };
 
+  // delete item from list
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   };
 
+  // + button for add new item to list
   const handleSubmit = (e) => {
-    e.preventDefault()
+    // prevent webpage reloading because html form
+    e.preventDefault();
+    // if no new item then exit function
     if (!newItem) return;
-  
-    // addItem - sets state to empty after submitted
-    setNewItem('')
+    addItem(newItem);
+    // sets state to empty after submitted
+    setNewItem("");
   };
+
   return (
     <div className="App">
       <Header title="Grocery List" />
@@ -66,8 +68,11 @@ function App() {
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
+      <SearchItem search={search} setSearch={setSearch} />
       <Content
-        items={items}
+        items={items.filter(item => {
+          return ((item.item).toLowerCase()).includes(search.toLowerCase())
+        })}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
